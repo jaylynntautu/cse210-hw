1bca3fd5e3e5024081bbdc2994611f7907fe0fc0
jaylynntautu <tau24009@byui.edu>
2025-10-26 20:55:56 -1100

    Final Journal Program - W04 Prove

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,57 @@
+// Journal Program - W04 Prove
+// Exceeded core requirements by implementing random prompt generator and file save/load
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Journal journal = new Journal();
+        PromptGenerator promptGenerator = new PromptGenerator();
+
+        string choice = "";
+        while (choice != "5")
+        {
+            Console.WriteLine("Please choose one of the following options:");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+            Console.Write("What would you like to do? ");
+            choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                string prompt = promptGenerator.GetRandomPrompt();
+                Console.WriteLine(prompt);
+                Console.Write(">");
+                string response = Console.ReadLine();
+
+                Entry newEntry = new Entry();
+                newEntry._date = DateTime.Now.ToShortDateString();
+                newEntry._prompt = prompt;
+                newEntry._response = response;
+                journal.AddEntry(newEntry);
+            }
+            else if (choice == "2")
+            {
+                journal.DisplayAll();
+            }
+            else if (choice == "3")
+            {
+                Console.Write("What is the filename? ");
+                string filename = Console.ReadLine();
+                journal.LoadFromFile(filename);
+            }
+            else if (choice == "4")
+            {
+                Console.Write("What is the filename? ");
+                string filename = Console.ReadLine();
+                journal.SaveToFile(filename);
+            }
+        }
+
+        Console.WriteLine("Goodbye!");
     }
 }
